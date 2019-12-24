7bb037da53d2922cb1b62f5970335c729a77ea5a
hindfh <hfmss77@hotmail.com>
2019-12-24 09:28:00 +0300

    Implement a Custom Control

---
+++ goalpost-app/goalpost-app/Controller/GoalsVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goalpost-app
+//
+//  Created by hind on 23/12/2019.
+//  Copyright © 2019 hind. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ goalpost-app/goalpost-app/View/GoalCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by hind on 23/12/2019.
+//  Copyright © 2019 hind. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
